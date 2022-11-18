69beb4c3170b8c06fccf905f3fe8cca220eed1e6
Sebastian Oteiza <oteiza.sebastian@gmail.com>
2022-11-17 21:38:32 -0300

    creacion de proyecto en grupo

---
+++ src/main/java/cl/infoclub/fsj/LogInFsj0045Application.java
@@ -0,0 +1,13 @@
+package cl.infoclub.fsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInFsj0045Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogInFsj0045Application.class, args);
+	}
+
+}
